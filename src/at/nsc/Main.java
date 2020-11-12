d8c2be08305296b6626f212d2fe97babd2f4b8fb
Niklas Schachl <niklas.schachl@htl-wels.at>
2020-11-12 11:01:38 +0100

    Added Converter Logo

--- src/at/nsc/Main.java
+++ src/at/nsc/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Übung 7");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
     public static void main(String[] args) {
